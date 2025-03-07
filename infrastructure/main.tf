1f2e688850622eb68054d76671ebcfe8abcb536e
B-Crolly <crol0005@algonquinlive.com>
2025-03-07 11:31:02 -0500

    fix: improve pre-commit hook and add required provider versions

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,8 +1,13 @@
 # Configure the Azure provider
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
@@ -22,7 +27,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "storage" {
   name                     = "ha09storage${random_string.random.result}"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                = azurerm_resource_group.rg.location
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
