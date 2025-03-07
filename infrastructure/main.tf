3f24519bfb5ffaca864a6335170a313d3f6a7395
B-Crolly <crol0005@algonquinlive.com>
2025-03-07 11:17:29 -0500

    test husky working terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,35 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "ha09-resources"
+  location = "canadacentral"
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "ha09storage${random_string.random.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create a random string for unique storage account name
+resource "random_string" "random" {
+  length  = 8
+  special = false
+  upper   = false
+}
